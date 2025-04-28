56c05541d9bb001f26aa9fc144f6e894790a4768
Juti <jbuican19@gmail.com>
2025-04-28 09:50:27 +0100

    add component function  and use h() to generate html , add test

--- packages/runtime/src/h.js
+++ packages/runtime/src/h.js
@@ -29,3 +29,24 @@ export function hFragment(vNodes) {
 export function hString(str) {
   return { type: DOM_TYPES.TEXT, value: str };
 }
+
+export function lipsum(num) {
+  const text =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in auctor mi, a cursus dui. Fusce molestie venenatis metus nec finibus.';
+  return hFragment(Array(num).fill(h('p', {}, [text])));
+}
+
+// return the a virtual dom that represents a message box with corresponding css class level ex. message--info
+export function MessageComponent({level, message}) {
+  return h('div',{ class: `message message--${level}` }, [
+    h('p', {}, [message])
+  ]);
+};
+
+// test
+h('h1', { class: 'title' }, ['My counter']);
+h('div', { class: 'container' }, [
+  h('button', {}, ['decrement']),
+  h('span', {}, ['0']),
+  h('button', {}, ['increment']),
+]);
